b7cf2155dc0b76aa375dd03f34cb58d47eb864b1
kubirthan <kubirthan7991@gmail.com>
2024-08-17 11:35:45 +0530

    frontend setup

---
+++ frontend/src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { Button } from './components/ui/button'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+   <Button>Click me</Button>
+  )
+}
+
+export default App
